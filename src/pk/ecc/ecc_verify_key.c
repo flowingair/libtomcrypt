65b4306aacb5722f2e81686f5c8c3bd0bbd4df8c
Karel Miko <karel.miko@gmail.com>
2017-03-28 10:30:12 +0200

    cosmetics

--- src/pk/ecc/ecc_verify_key.c
+++ src/pk/ecc/ecc_verify_key.c
@@ -30,15 +30,15 @@ int ecc_verify_key(ecc_key *key)
   void *prime = NULL;
   void *order = NULL;
   void *a = NULL;
-  ecc_point *test_output = NULL;
-  test_output = XMALLOC(sizeof(ecc_point));
+  ecc_point *test_out = NULL;
+  test_out = XMALLOC(sizeof(ecc_point));
 
-  if (mp_init_multi(&(test_output->x), &(test_output->y), &(test_output->z), &order, &prime, NULL) != CRYPT_OK) {
+  if (mp_init_multi(&(test_out->x), &(test_out->y), &(test_out->z), &order, &prime, NULL) != CRYPT_OK) {
     return CRYPT_MEM;
   }
 
   /* Test 1: Are the x amd y points of the public key in the field? */
-  if((err = ltc_mp.read_radix(prime, key->dp->prime, 16)) != CRYPT_OK)                  { goto error;}
+  if((err = ltc_mp.read_radix(prime, key->dp->prime, 16)) != CRYPT_OK)                  { goto error; }
 
   if(ltc_mp.compare_d(key->pubkey.z, 1) == LTC_MP_EQ) {
     if(
@@ -53,18 +53,16 @@ int ecc_verify_key(ecc_key *key)
   }
 
   /* Test 2: is the public key on the curve? */
-  if((err = ltc_ecc_is_point(key->dp, key->pubkey.x, key->pubkey.y)) != CRYPT_OK)       { goto error;}
+  if((err = ltc_ecc_is_point(key->dp, key->pubkey.x, key->pubkey.y)) != CRYPT_OK)       { goto error; }
 
   /* Test 3: does nG = O? (n = order, 0 = point at infinity, G = public key) */
-  if((err = ltc_mp.read_radix(order, key->dp->order, 16)) != CRYPT_OK)                  { goto error;}
-  if((err = ltc_mp.read_radix(a, key->dp->A, 16)) != CRYPT_OK)                          { goto error;}
-  if((err = ltc_ecc_mulmod(order, &(key->pubkey), test_output, a, prime, 1)) != CRYPT_OK) {
-    goto error;
-  }
+  if((err = ltc_mp.read_radix(order, key->dp->order, 16)) != CRYPT_OK)                  { goto error; }
+  if((err = ltc_mp.read_radix(a, key->dp->A, 16)) != CRYPT_OK)                          { goto error; }
+  if((err = ltc_ecc_mulmod(order, &(key->pubkey), test_out, a, prime, 1)) != CRYPT_OK)  { goto error; }
 
   err = CRYPT_OK;
 error:
-  mp_clear_multi(prime, order, test_output->z, test_output->y, test_output->x, NULL);
+  mp_clear_multi(prime, order, test_out->z, test_out->y, test_out->x, NULL);
   return err;
 }
 
